535f49dbe76cacc21c1b36f9c94af62b2eb2633c
Bastian8004 <paszko08042004@gmail.com>
2023-02-23 22:16:53 +0100

    css prawie gotowy, modyfikacja stron oraz dodanie nowych (href)

--- qumaszpage/urls.py
+++ qumaszpage/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('', views.uslugi, name='uslugi'),
+    path('', views.kontakt, name='kontakt'),
+    path('', views.onas, name='onas'),
+    path('', views.opinie, name='opinie'),
+    path('', views.cennik, name='cennik'),
+    path('', views.conowego, name='conowego'),
 ]
--- qumaszpage/views.py
+++ qumaszpage/views.py
@@ -7,3 +7,21 @@ from django.http import HttpResponse
 
 def main(request):
     return render(request, 'main.html')
+
+def uslugi(request):
+    return render(request, 'uslugi.html')
+
+def kontakt(request):
+    return render(request, 'kontakt.html')
+
+def cennik(request):
+    return render(request, 'cennik.html')
+
+def conowego(request):
+    return render(request, 'conowego.html')
+
+def onas(request):
+    return render(request, 'onas.html')
+
+def opinie(request):
+    return render(request, 'opinie.html')
